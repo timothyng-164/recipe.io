7b6114513e6bfac26e9f7b7242bef63742e1b017
yroush <32183395+yroush@users.noreply.github.com>
2019-04-03 01:54:21 -0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
-  // $("#search-recipe-container").hide();
-  $("#add-recipe-container").hide();
+  // $('#search-recipe-container').hide();
+  $('#add-recipe-container').hide();
+
+  $.getJSON('../data/recipes.json', function (data) {
+    console.log(data);
+  });
 });
 
-
-
-$('#recipe-search-btn').on("click", function() {
-  $("#search-recipe-container").show();
-  $("#add-recipe-container").hide();
+$('#recipe-search-btn').on('click', function () {
+  $('#search-recipe-container').show();
+  $('#add-recipe-container').hide();
 });
 
-$('#recipe-add-btn').on("click", function() {
-  $("#search-recipe-container").hide();
-  $("#add-recipe-container").show();
+$('#recipe-add-btn').on('click', function () {
+  $('#search-recipe-container').hide();
+  $('#add-recipe-container').show();
 });
