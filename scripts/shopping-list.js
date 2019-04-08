4f595436c1364aa23afca3d1cebf033e7202f224
yroush <32183395+yroush@users.noreply.github.com>
2019-04-07 21:47:24 -0700

    Add files via upload
    linter error fixes

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
-  load_tables();
+  loadTables();
 });
 
-
 // populate recipe and ingredient tables
-function load_tables() {
-  $('tbody *').remove();  // clear all table rows
-  var local_recipe_list = JSON.parse(localStorage.getItem('my_recipe_list'));
+function loadTables () {
+  $('tbody *').remove();// clear all table rows
+  var localRecipeList = JSON.parse(localStorage.getItem('my_recipe_list'));
 
-  $.getJSON('../data/recipes.json', function (all_recipes) {
-    for (var id in local_recipe_list) {
-      var recipe = all_recipes.find(recipe => recipe.id == id);
+  $.getJSON('../data/recipes.json', function (allRecipes) {
+    for (var id in localRecipeList) {
+      var recipe = allRecipes.find(recipe => recipe.id === parseInt(id));
       // show recipe in table
       $('#recipe-list-table tbody').append(
         '<tr>' +
-          '<td>' + recipe.name +'</td>' +
-          '<td>' + local_recipe_list[id] +'</td>' +
+          '<td>' + recipe.name + '</td>' +
+          '<td>' + localRecipeList[id] + '</td>' +
           '<td><button class="remove-recipe" recipe_id="' + id + '">x</button></td>' +
         '</tr>'
       );
@@ -24,24 +23,22 @@ function load_tables() {
       var ingredients = recipe.ingredients;
       for (var i in ingredients) {
         $('#ingredient-list-table tbody').append(
-        '<tr>' +
-          '<td><input type="checkbox"></td>' +
-          '<td>' + ingredients[i].quantity + '</td>' +
-          '<td>' + ingredients[i].name+ '</td>' +
-        '</tr>'
+          '<tr>' +
+            '<td><input type="checkbox"></td>' +
+            '<td>' + ingredients[i].quantity + '</td>' +
+            '<td>' + ingredients[i].name + '</td>' +
+          '</tr>'
         );
       }
     }
   });
 }
 
-
 // remove recipe when pressing 'x' button
 $(document).on('click', '.remove-recipe', function (event) {
-  var local_recipe_list = JSON.parse(localStorage.getItem('my_recipe_list'));
+  var localRecipeList = JSON.parse(localStorage.getItem('my_recipe_list'));
   var id = $(this).attr('recipe_id');
-  delete local_recipe_list[id];
-  localStorage.setItem('my_recipe_list', JSON.stringify(local_recipe_list));
-  load_tables();
-
+  delete localRecipeList[id];
+  localStorage.setItem('my_recipe_list', JSON.stringify(localRecipeList));
+  loadTables();
 });
